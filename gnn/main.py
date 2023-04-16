751c04bdd0be30af02bf82a80ee69d66dee5a3cb
1110236 <915840469@qq.com>
2023-04-16 16:52:47 +0700

    Small update about using the correct acc calculator - divide by dataloader._indices; Separate the train and test function out for iterative uses.

--- gnn/main.py
+++ gnn/main.py
@@ -13,23 +13,7 @@ from dgldataset import HandGestureDataset
 from dgl.data import citegrh
 
 
-if __name__ == '__main__':
-
-    # Create the dgl dataset
-    hand_gesture_dataset = HandGestureDataset()
-
-    # Define data loader
-    hand_gesture_dataloader = HandGestureDataLoader(dataset=hand_gesture_dataset)
-    train_loader = hand_gesture_dataloader.get_train_loader()
-    test_loader = hand_gesture_dataloader.get_test_loader()
-    val_loader = hand_gesture_dataloader.get_val_loader()
-
-    # A random GNN model
-    num_node_features = hand_gesture_dataset.num_node_features      # Get the number of features for each node - 3
-    num_classes = hand_gesture_dataset.num_classes                  # Get the number of classes - 36
-    model = GraphSAGEModel(in_feats=num_node_features, n_hidden=32, out_dim=num_classes,
-                           n_layers=2, activation=F.relu, dropout=0.5, aggregator_type='gcn')
-
+def train(model, train_loader, val_loader):
     # Define optimizer and loss function
     optimizer = Adam(model.parameters(), lr=0.01, weight_decay=0.01)
     criterion = CrossEntropyLoss()
@@ -60,7 +44,6 @@ if __name__ == '__main__':
         model.eval()
         total_val_loss = 0.0
         total_val_acc = 0.0
-        num_val_batches = 0
         with torch.no_grad():
             for batch_idx, (bg, labels) in enumerate(val_loader):
                 # Forward pass
@@ -75,20 +58,26 @@ if __name__ == '__main__':
                 predicted = torch.argmax(logits, dim=1)
                 true_labels = torch.argmax(labels, dim=1)
                 # print(f'The predicted is: {predicted}, the true_label is: {true_labels}')
-                # print(f'The correct number was: {(predicted == true_labels).sum()}, the number of all candidates is: {len(true_labels)}')
-                total_val_acc += ((predicted == true_labels).sum()/len(true_labels)).item()
+                # print(f'The correct number was: {(predicted == true_labels).sum()}, the number of all candidates is: {len(true_labels)}, the number of val_loader is: {len(val_loader)}')
+                total_val_acc += ((predicted == true_labels).sum()).item()
 
             avg_val_loss = total_val_loss / len(val_loader)
-            avg_val_acc = total_val_acc / len(val_loader)
+            avg_val_acc = total_val_acc / len(hand_gesture_dataloader.val_indices)
 
             # Print training and validation loss for the epoch
-            print('Epoch {}, Train Loss {:.4f}, Val Loss {:.4f}, Val Accuracy {:.4f}'.format(epoch, total_train_loss / len(train_loader), avg_val_loss, avg_val_acc))
+            print('Epoch {}, Train Loss {:.4f}, Val Loss {:.4f}, Val Accuracy {:.4f}'.format(epoch,
+                                                                                             total_train_loss / len(
+                                                                                                 train_loader),
+                                                                                             avg_val_loss, avg_val_acc))
+    return model
 
+
+def test(model, test_loader):
+    criterion = CrossEntropyLoss()
     # Evaluate on test set
     model.eval()
     total_test_loss = 0.0
     total_test_acc = 0.0
-    num_test_batches = 0
     with torch.no_grad():
         for batch_idx, (bg, labels) in enumerate(test_loader):
             # Forward pass
@@ -102,10 +91,33 @@ if __name__ == '__main__':
             # Compute accuracy
             predicted = torch.argmax(logits, dim=1)
             true_labels = torch.argmax(labels, dim=1)
-            total_test_acc += ((predicted == true_labels).sum()/len(true_labels)).item()
+            total_test_acc += ((predicted == true_labels).sum()).item()
 
         avg_test_loss = total_test_loss / len(test_loader)
-        avg_test_acc = total_test_acc / len(test_loader)
+        avg_test_acc = total_test_acc / len(hand_gesture_dataloader.test_indices)
 
         print('Test Loss {:.4f}, Test Accuracy {:.4f}'.format(avg_test_loss, avg_test_acc))
 
+
+if __name__ == '__main__':
+
+    # Create the dgl dataset
+    hand_gesture_dataset = HandGestureDataset()
+
+    # Define data loader
+    hand_gesture_dataloader = HandGestureDataLoader(dataset=hand_gesture_dataset)
+    train_loader = hand_gesture_dataloader.get_train_loader()
+    test_loader = hand_gesture_dataloader.get_test_loader()
+    val_loader = hand_gesture_dataloader.get_val_loader()
+
+    num_node_features = hand_gesture_dataset.num_node_features      # Get the number of features for each node - 3
+    num_classes = hand_gesture_dataset.num_classes                  # Get the number of classes - 36
+
+    # Create the model
+    graphsage = GraphSAGEModel(in_feats=num_node_features, n_hidden=32, out_dim=num_classes,
+                           n_layers=2, activation=F.relu, dropout=0.5, aggregator_type='gcn')
+
+    # Train the model
+    graphsage_trained_model = train(graphsage, train_loader, val_loader)
+    # Test the model
+    test(graphsage_trained_model, test_loader)
